c69c1b46dd15d6211f94359de1cf5c5b194b55a5
takashi <takashi.sh@proton.me>
2024-09-16 03:31:34 -0700

    unique seeds in random choice, no more duplicates

--- app/seeds/artifax.py
+++ app/seeds/artifax.py
@@ -1,6 +1,6 @@
 # seeds/artifax_seeder.py
 from sqlalchemy.sql import text
-from random import choice
+from random import sample, choice
 from app.models import db, Artifax, environment, SCHEMA
 from . import seed_data as sd
 
@@ -8,13 +8,17 @@ from . import seed_data as sd
 def seed_artifax():
     """Seed the artifax table with data"""
 
+    img_urls = sample(sd.artifax_img_urls, sd.num_artifax)
+    titles = sample(sd.artifax_titles, sd.num_artifax)
+    descriptions = sample(sd.artifax_descriptions, sd.num_artifax)
+
     for i in range(sd.num_artifax):
         print(f"Seeding artifax {i + 1} of {sd.num_artifax}")
         artifax = Artifax(
             owner_id=choice(range(1, len(sd.users_list) + 1)),
-            image=choice(sd.artifax_img_urls),
-            title=choice(sd.artifax_titles),
-            description=choice(sd.artifax_descriptions),
+            image=img_urls[i],
+            title=titles[i],
+            description=descriptions[i],
         )
         db.session.add(artifax)
     db.session.commit()
